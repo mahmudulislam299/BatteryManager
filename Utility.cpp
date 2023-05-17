2000e26f13324d5017eb5b487dfbc5f6acc0d957
mahmudulislam299 <mahmudulislam299@gmail.com>
2023-05-17 15:19:56 +0600

    position check function added

--- Utility.cpp
+++ Utility.cpp
@@ -1,1 +1,10 @@
 #include "Utility.h"
+
+char postionCheckFunc(char *arr, int postion, int num_byte)
+{
+	for(int i =0 ; i < num_byte; i++)
+	{
+		Serial.print(arr[postion+i]);
+	}
+	Serial.println();
+}
--- Utility.h
+++ Utility.h
@@ -1,4 +1,10 @@
 #ifndef UTILITY_H
 #define UTILITY_h
 
+#include <Arduino.h>
+// #include "BatteryCommand.h"
+
+
+char postionCheckFunc(char *arr, int postion, int num_byte= 4);
+
 #endif // !UTILITY_H
