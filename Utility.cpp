7749ac5a13c27a7d3ff8c70579c42658ecb6cadf
mahmudulislam299 <mahmudulislam299@gmail.com>
2023-05-17 18:28:38 +0600

    extract subArray func added

--- Utility.cpp
+++ Utility.cpp
@@ -8,3 +8,29 @@ char postionCheckFunc(char *arr, int postion, int num_byte)
 	}
 	Serial.println();
 }
+
+
+void extractSubarray(const char* input, int startPos, int size, char* output) 
+{
+  int endPos = startPos + size;
+
+  // Extract the subarray
+  int j = 0;
+  for (int i = startPos; i < endPos; i++) {
+    output[j] = input[i];
+    j++;
+  }
+  output[j] = '\0'; // Null-terminate the output array
+}
+
+
+
+// void loop() {
+//   char arr[] = "~250F46003076000";
+//   char output_arr[10]; // Adjust the size as per your requirement
+
+//   extractSubarray(arr, 1, 4, output_arr);
+
+//   Serial.println(output_arr); // Print the extracted subarray
+//   delay(1000);
+// }
--- Utility.h
+++ Utility.h
@@ -4,5 +4,7 @@
 #include <Arduino.h>
 
 char postionCheckFunc(char *arr, int postion, int num_byte= 4);
+void extractSubarray(const char* input, int startPos, int size, char* output) ;
+
 
 #endif // !UTILITY_H
